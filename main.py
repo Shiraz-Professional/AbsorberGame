9e8bf0ece54c9e28af123676ba1b672c8d7b48cf
Shiraz-Professional <166607168+Shiraz-Professional@users.noreply.github.com>
2025-02-03 15:05:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,24 @@ class Creature:
         self.y = (self.y + self.yspeed) % 1800
         moveSprite(self.sprite, self.x, self.y, centre=True)
 
+class Player(Creature):
+    def move(self, creatures):
+        #work out the angle from the player to the mouse
+        dx = mouseX() - self.x
+        dy = mouseY() - self.y
+        dist = math.sqrt(dx**2 + dy**2)
+        self.speed = dist /200 *10
+
+        self.angle = math.degrees(math.atan2(dy, dx))
+        transformSprite(self.sprite,self.angle, self.size/100)
+        super().move()
+        for c in creatures:
+            if touching(self.sprite,c.sprite):
+                if self.size > c.size:
+                    print("Nom")
+                    creatures.remove(c)
+                    hideSprite(c.sprite)
+                    self.size += 5
 
 
 setAutoUpdate(False)
@@ -33,9 +51,12 @@ for i in range(20):
     c1 = Creature(random.randint(0,1000),random.randint(0,1000), "enemy.png", random.randint(10,100)) 
     creatures.append(c1)
 
+p = Player(500,500,"player.png",20)
+
 while True:
     for creature in creatures:
         creature.move()
+    p.move(creatures)
     updateDisplay()
     tick(50)
    
